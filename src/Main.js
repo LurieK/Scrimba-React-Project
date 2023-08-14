f6dba81ae8e1df9c7298208228aa76f64ae79962
Lurie <lurainekimmerle@gmail.com>
2023-08-14 04:30:15 +0000

    add content to Main.js, add font color, background image, spacing to Main.js

--- src/Main.js
+++ src/Main.js
@@ -2,7 +2,16 @@
 
 function Main (){
     return (
-        <h1>Main goes here</h1>
+        <div className="main-content">
+            <h1 className="list-header">Fun facts about React</h1>
+            <ul className="list-items">
+                <li>Was first released in 2013</li>
+                <li>Was origionally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
     )
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -6,7 +6,7 @@ function Navbar (){
         <nav className="navbar" >
             <div className="logo">
                 <img className="logo-img" alt="logo" src={logo}/>
-                <h3 className="logo-heading">React Facts</h3>
+                <h3 className="logo-heading">ReactFacts</h3>
             </div>
             <h4 className="subheading">React Course- Project 1</h4>   
         </nav>
