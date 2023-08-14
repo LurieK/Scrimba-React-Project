7cc6bc13d27cfea783ca9b194b6885091ba61d64
Lurie <lurainekimmerle@gmail.com>
2023-08-14 03:07:42 +0000

    Create outline for Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,13 @@
 
 function Navbar (){
     return(
-        <h1>Navbar goes here</h1>
+        <nav className="navbar">
+            <div className="logo">
+                <img className="nav-img"/>
+                <h3 className="nav-heading">React</h3>
+            </div>
+            <h4 className="nav-subheading">React Course- Project 1</h4>   
+        </nav>
     )
 }
 
