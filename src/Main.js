a05f0667aaa9cd859bbdd03a6589b2c9faadf8b3
Lurie <lurainekimmerle@gmail.com>
2023-08-14 05:31:22 +0000

    add span to li and set bullet color and font color distinction

--- src/Main.js
+++ src/Main.js
@@ -5,11 +5,11 @@ function Main (){
         <div className="main-content">
             <h1 className="list-header">Fun facts about React</h1>
             <ul className="list-items">
-                <li>Was first released in 2013</li>
-                <li>Was origionally created by Jordan Walke</li>
-                <li>Has well over 100K stars on GitHub</li>
-                <li>Is maintained by Facebook</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
+                <li><span>Was first released in 2013</span></li>
+                <li><span>Was origionally created by Jordan Walke</span></li>
+                <li><span>Has well over 100K stars on GitHub</span></li>
+                <li><span>Is maintained by Facebook</span></li>
+                <li><span>Powers thousands of enterprise apps, including mobile apps</span></li>
             </ul>
     
         </div>
